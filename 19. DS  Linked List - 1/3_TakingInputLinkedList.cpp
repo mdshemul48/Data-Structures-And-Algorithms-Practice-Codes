2918458c23aab893b5575bd2971a78498a4bd7bd
MD. Shimul <mdshemul480@gmail.com>
2021-12-08 01:22:23 +0600

    taking input and printing

--- 19. DS  Linked List - 1/3_TakingInputLinkedList.cpp
+++ 19. DS  Linked List - 1/3_TakingInputLinkedList.cpp
@@ -2,8 +2,68 @@
 
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL; // LL is empty
+    Node *tail = NULL; // LL is empty
+
+    // we will stop taking input if user enter -1 in input
+    while (data != -1)
+    {
+        // creating linkedList
+        Node *n = new Node(data);
+        // checking if first node or not.
+
+        // if the head element is null then our linkedList is empty.
+        // or if we have something other then null in head that's mean our list have some value
+        // In that case we will add value to the end (tail)
+        // after adding to the end. we will update tail to the last element (n element).
+
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
 int main()
 {
-    
+    Node *head = takeInput();
+    print(head);
+
     return 0;
 }
