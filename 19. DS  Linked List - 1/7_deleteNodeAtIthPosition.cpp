476ba53d85c8b10f3c7d87ad3ecbd8cd0a859288
MD. Shimul <mdshemul480@gmail.com>
2021-12-10 01:47:06 +0600

    adding delete function for linked list

---
+++ 19. DS  Linked List - 1/7_deleteNodeAtIthPosition.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+Node *deleteNodeAtIthPosition(Node *head, int i)
+{
+
+    /*
+    first of all we are checking if our index is valid or not.
+    after that if index is 0 then we will make it next node head.
+    that will delete our head node.
+    ----------
+    otherwise we will go to i-1 position and replace i-1 position node next pointer with
+    i-1->next->next node. that will delete iTh node.
+    */
+
+    if (i < 0)
+    {
+        return head;
+    }
+    if (i == 0)
+    {
+        head = head->next;
+        return head;
+    }
+
+    int count = 1;
+    Node *temp = head;
+    while (count <= i - 1 and temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp && temp->next)
+    {
+        temp->next = temp->next->next;
+        return head;
+    }
+
+    return head;
+}
+
+int main()
+{
+    // this is passing input to console..
+    freopen("input.txt", "r", stdin);
+
+    Node *head = takeInput();
+    head = deleteNodeAtIthPosition(head, 0);
+    print(head);
+
+    return 0;
+}
