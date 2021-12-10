ec69f27a3681006e07a2fe4691d681d03db00a9a
MD. Shimul <mdshemul480@gmail.com>
2021-12-11 01:06:46 +0600

    setting up files

---
+++ 20. DS  Linked List - 2/LinkListLengthRecursive.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+
+
+int main()
+{
+
+    freopen("input.txt", "r", stdin);
+    Node *head = takeInput();
+    print(head);
+
+    return 0;
+}
