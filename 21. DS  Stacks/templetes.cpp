04b788930929fad71e78fac6845a2cde13ca31d5
MD. Shimul <mdshemul480@gmail.com>
2021-12-26 02:25:14 +0600

    adding 2 type in pair

--- 21. DS  Stacks/templetes.cpp
+++ 21. DS  Stacks/templetes.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 // creating dynamic type with template.
-template <typename T>
+template <typename T, typename V>
 class Pair
 {
     T x;
-    T y;
+    V y;
 
 public:
     void setX(T x)
@@ -18,11 +18,11 @@ public:
         return x;
     }
 
-    void setY(T y)
+    void setY(V y)
     {
         this->y = y;
     }
-    T getY()
+    V getY()
     {
         return y;
     }
@@ -32,7 +32,7 @@ int main()
 {
 
     // int
-    Pair<int> f1;
+    Pair<int, int> f1;
 
     f1.setX(5);
     f1.setY(6);
@@ -41,10 +41,16 @@ int main()
     cout << f1.getY() << endl;
 
     // double
-    Pair<double> f2;
+    Pair<double, double> f2;
     f2.setX(5.1212);
     f2.setY(1.3434);
     cout << f2.getX() << " " << f2.getY() << endl;
+
+    // int and char togathar
+    Pair<int, char> f3;
+    f3.setX(1);
+    f3.setY('g');
+    cout << f3.getX() << " " << f3.getY() << endl;
 
     return 0;
 }
