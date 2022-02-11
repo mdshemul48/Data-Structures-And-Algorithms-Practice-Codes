ea26fa171a8cb31adba5ab300f7bdc30fcee629b
MD. Shimul <mdshemul480@gmail.com>
2022-02-11 16:24:12 +0600

    getting input using loop

--- 23. DS  Generic Trees/TakeInputLavelWise.cpp
+++ 23. DS  Generic Trees/TakeInputLavelWise.cpp
@@ -32,7 +32,39 @@ void printTree(TreeNode<int> *root)
     }
 }
 
-int main()
+TreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int> *> q;
+
+    q.push(root);
+
+    while (!q.empty())
+    {
+        TreeNode<int> *f = q.front();
+        q.pop();
+        cout << "Enter no of children of " << f->data << endl;
+        int n;
+        cin >> n;
+
+        for (int i = 0; i <= n; i++)
+        {
+            int childData;
+            cout << "Enter " << i << " th child of " << f->data << endl;
+            cin >> childData;
+
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            q.push(child);
+            f->children.push_back(child);
+        }
+    }
+}
+
+main()
 {
 
     return 0;
