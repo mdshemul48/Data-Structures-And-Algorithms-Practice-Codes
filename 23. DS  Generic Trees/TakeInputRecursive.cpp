3257ceaf5d55dfa1873160ecb65629e60be8886d
MD. Shimul <mdshemul480@gmail.com>
2022-02-11 13:28:21 +0600

    adding print func

--- 23. DS  Generic Trees/TakeInputRecursive.cpp
+++ 23. DS  Generic Trees/TakeInputRecursive.cpp
@@ -30,6 +30,25 @@ TreeNode<int> *takeInput()
     }
 }
 
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << ": ";
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << " ,";
+    }
+    cout << endl;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
 int main()
 {
     TreeNode<int> *root = new TreeNode<int>(1);
