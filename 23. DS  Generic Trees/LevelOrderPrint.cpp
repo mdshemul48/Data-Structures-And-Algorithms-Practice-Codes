f6c8f11dcf6b300add494770c047f479f2108e3a
MD. Shimul <mdshemul480@gmail.com>
2022-02-16 20:44:18 +0600

    adding lavel wise print

--- 23. DS  Generic Trees/LevelOrderPrint.cpp
+++ 23. DS  Generic Trees/LevelOrderPrint.cpp
@@ -1,8 +1,71 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main()
+template <typename T>
+class TreeNode
 {
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << ": ";
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << " ,";
+    }
+    cout << endl;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+
+TreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    TreeNode<int> *root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int> *> q;
+
+    q.push(root);
+
+    while (!q.empty())
+    {
+        TreeNode<int> *f = q.front();
+        q.pop();
+        cout << "Enter no of children of " << f->data << endl;
+        int n;
+        cin >> n;
+
+        for (int i = 0; i <= n; i++)
+        {
+            int childData;
+            cout << "Enter " << i << " th child of " << f->data << endl;
+            cin >> childData;
+
+            TreeNode<int> *child = new TreeNode<int>(childData);
+            q.push(child);
+            f->children.push_back(child);
+        }
+    }
+}
+
+main()
+{
+
     return 0;
 }
