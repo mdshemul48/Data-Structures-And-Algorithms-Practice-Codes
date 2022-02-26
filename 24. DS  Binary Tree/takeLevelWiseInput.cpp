b16cbf065784d3c4931932c1d3c9c8f31c4937d0
MD. Shimul <mdshemul480@gmail.com>
2022-02-26 21:34:14 +0600

    adding new code

---
+++ 24. DS  Binary Tree/takeLevelWiseInput.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class BTNode
+{
+public:
+    T data;
+    BTNode *left;
+    BTNode *right;
+    BTNode(T data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+    ~BTNode()
+    {
+        delete left;
+        delete right;
+    }
+};
+
+void printTree(BTNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << ": ";
+
+    if (root->left != NULL)
+    {
+        cout << "L" << root->left->data;
+    }
+
+    if (root->right != NULL)
+    {
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
+
+    printTree(root->left);
+    printTree(root->right);
+}
+
+int main()
+{
+
+    return 0;
+}
