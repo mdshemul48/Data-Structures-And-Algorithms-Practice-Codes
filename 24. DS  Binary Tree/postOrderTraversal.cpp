f0ad17e404b40273679a9a37eee1ecb59a1e159c
MD. Shimul <mdshemul480@gmail.com>
2022-02-27 21:06:21 +0600

    adding post order traversal

---
+++ 24. DS  Binary Tree/postOrderTraversal.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+class BTNode
+{
+public:
+    T data;
+    BTNode *left;
+    BTNode *right;
+    BTNode(T data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+    ~BTNode()
+    {
+        delete left;
+        delete right;
+    }
+};
+
+void printTree(BTNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << ": ";
+
+    if (root->left != NULL)
+    {
+        cout << "L" << root->left->data;
+    }
+
+    if (root->right != NULL)
+    {
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
+
+    printTree(root->left);
+    printTree(root->right);
+}
+BTNode<int> *takeInputlevelWise()
+{
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    BTNode<int> *root = new BTNode<int>(rootData);
+    queue<BTNode<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        BTNode<int> *f = q.front();
+        q.pop();
+        cout << "Enter Left child of " << f->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BTNode<int> *child = new BTNode<int>(leftChildData);
+            q.push(child);
+            f->left = child;
+        }
+
+        cout << "Enter Right child of " << f->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+
+        if (rightChildData != -1)
+        {
+            BTNode<int> *child = new BTNode<int>(rightChildData);
+            q.push(child);
+            f->right = child;
+        }
+    }
+    return root;
+}
+
+//==========================================
+// aikane amara first a right then left then root order print kortaci.
+// aita holo postOrderTreversal
+
+void preOrderTraversal(BTNode<int> *root)
+{
+    if (!root)
+        return;
+
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+    cout << root->data << endl;
+}
+
+int main()
+{
+
+    BTNode<int> *root = takeInputlevelWise();
+    preOrderTraversal(root);
+    return 0;
+}
