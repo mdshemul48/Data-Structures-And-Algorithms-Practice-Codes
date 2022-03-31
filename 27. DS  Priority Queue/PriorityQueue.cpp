7c020c9fab9c244386d5217fefe0e48bd901628a
MD. Shimul <mdshemul480@gmail.com>
2022-03-31 21:12:03 +0600

    adding priorityQueue remove func

--- 27. DS  Priority Queue/PriorityQueue.cpp
+++ 27. DS  Priority Queue/PriorityQueue.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// min heap
+
 class PriorityQueue
 {
     vector<int> pq;
@@ -24,6 +26,12 @@ public:
 
         return pq[0];
     }
+
+    int getSize()
+    {
+        return pq.size();
+    }
+
     void insert(int element)
     {
         pq.push_back(element);
@@ -42,10 +50,70 @@ public:
             }
         }
     }
+
+    int removeMin()
+    {
+        if (isEmpty())
+        {
+            return 0;
+        }
+
+        int ans = pq[0];
+
+        swap(pq[0], pq[pq.size() - 1]);
+        pq.pop_back();
+
+        // CBT yes, Heap no
+        // down heapify
+
+        int pi = 0;
+
+        while (true)
+        {
+            int lci = 2 * pi + 1;
+            int rci = 2 * pi + 1;
+            int mini = pi;
+
+            if (lci < pq.size() && pq[lci] < pq[mini])
+            {
+                mini = lci;
+            }
+
+            if (rci < pq.size() && pq[rci] < pq[mini])
+            {
+                mini = rci;
+            }
+
+            if (mini == pi)
+            {
+                break;
+            }
+
+            swap(pq[pi], pq[mini]);
+            pi = mini;
+        }
+        return ans;
+    }
 };
 
 int main()
 {
+    PriorityQueue p;
 
+    p.insert(100);
+    p.insert(10);
+    p.insert(15);
+    p.insert(3);
+    p.insert(17);
+    p.insert(21);
+
+    cout << p.getSize() << endl;
+
+    cout << p.getMin() << endl;
+
+    while (!p.isEmpty())
+    {
+        cout << p.removeMin() << " ";
+    }
     return 0;
 }
