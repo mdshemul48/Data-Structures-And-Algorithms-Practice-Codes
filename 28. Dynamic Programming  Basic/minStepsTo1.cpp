648f1774d7ff31c63a067166b536b62b8004b7ac
MD. Shimul <mdshemul480@gmail.com>
2022-04-05 23:10:12 +0600

    adding halper func with memorization

--- 28. Dynamic Programming  Basic/minStepsTo1.cpp
+++ 28. Dynamic Programming  Basic/minStepsTo1.cpp
@@ -1,6 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// memorization with helper func
+int helper(int num, int *arr)
+{
+    if (num == 1)
+    {
+        return 0;
+    }
+
+    if (arr[num] != -1)
+    {
+        return arr[num];
+    }
+
+    int x = helper(num - 1, arr);
+    if (num % 2 == 0)
+    {
+        int j = helper(num / 2, arr);
+        x = min(x, j);
+    }
+
+    if (num % 3 == 0)
+    {
+        int j = helper(num / 3, arr);
+        x = min(j, x);
+    }
+    return arr[num] = x + 1;
+}
+
+int minSteps2(int n)
+{
+    int *ans = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        ans[i] = -1;
+    }
+}
+
+// memorization ->DP
 int minStepToOne(int num, vector<int> &arr)
 {
     if (num == 1)
