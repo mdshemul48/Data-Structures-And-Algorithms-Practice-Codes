4977f20bb9999660fe2f5e267e5a65d1e3e6a6d4
MD. Shimul <mdshemul480@gmail.com>
2022-04-06 22:24:58 +0600

    adding iterative one

--- 28. Dynamic Programming  Basic/climbStairs.cpp
+++ 28. Dynamic Programming  Basic/climbStairs.cpp
@@ -26,8 +26,24 @@ int countSteps2(int n)
     return halper(n, arr);
 }
 
+// iterative and dp
+
+int countStep3(int n)
+{
+    int *arr = new int[n + 1];
+    arr[0] = 1;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[n];
+}
+
 int main()
 {
     cout << countSteps2(5) << endl;
+    cout << countStep3(5) << endl;
     return 0;
 }
