8b0123a3e12122f0971069f689e6ef5bb78bfe02
MD. Shimul <mdshemul480@gmail.com>
2022-04-06 22:25:42 +0600

    adding kSmallestElement

---
+++ 27. DS  Priority Queue/kSmallestElement.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void kSmallest(int *a, int n, int k)
+{
+    priority_queue<int> pq;
+
+    for (int i = 0; i < k; i++)
+    {
+        pq.push(a[i]);
+    }
+
+    for (int i = k; i < n; i++)
+    {
+        if (a[i] < pq.top())
+        {
+            pq.pop();
+            pq.push(a[i]);
+        }
+    }
+
+    while (!pq.empty())
+    {
+        cout << pq.top() << endl;
+        pq.pop();
+    }
+}
+
+int main()
+{
+    int a[] = {5, 6, 9, 12, 3, 13, 2};
+
+    kSmallest(a, 7, 3);
+
+    return 0;
+}
