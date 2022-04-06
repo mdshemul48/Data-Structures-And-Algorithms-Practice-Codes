73c7d6e45cdc26d259899f0144950e8332aaea27
MD. Shimul <mdshemul480@gmail.com>
2022-04-06 22:17:51 +0600

    adding countSteps with halper func

--- 28. Dynamic Programming  Basic/climbStairs.cpp
+++ 28. Dynamic Programming  Basic/climbStairs.cpp
@@ -10,8 +10,24 @@ int countSteps(int n)
     return countSteps(n - 1) + countSteps(n - 2);
 }
 
+// count steps dp
+int halper(int n, int *arr)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
+    return arr[n] = halper(n - 1, arr) + halper(n - 2, arr);
+}
+
+int countSteps2(int n)
+{
+    int *arr = new int[n + 1];
+
+    return halper(n, arr);
+}
+
 int main()
 {
-    cout << countSteps(2) << endl;
+    cout << countSteps2(5) << endl;
     return 0;
 }
