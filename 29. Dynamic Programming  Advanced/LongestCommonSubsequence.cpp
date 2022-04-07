c39e668fb854bbc63ea941b42b152cc44ca93941
MD. Shimul <mdshemul480@gmail.com>
2022-04-07 21:18:33 +0600

    adding new dp problem

--- 29. Dynamic Programming  Advanced/LongestCommonSubsequence.cpp
+++ 29. Dynamic Programming  Advanced/LongestCommonSubsequence.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// with only recursion
 int lcs(string s1, int i, string s2, int j)
 {
     if (i == s1.size() || j == s2.size())
@@ -22,10 +23,38 @@ int longestCommonSubsequence(string text1, string text2)
     return lcs(text1, 0, text2, 0);
 }
 
+//  with dp
+
+int longestCommonSubsequence2(string text1, string text2)
+{
+    int m = text1.size();
+    int n = text2.size();
+    int dp[m + 1][n + 1];
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+            else if (text1[i] == text2[j])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
 int main()
 {
     string t1 = "abcde";
     string t2 = "ace";
-    cout << longestCommonSubsequence(t1, t2) << endl;
+    cout << longestCommonSubsequence2(t1, t2) << endl;
     return 0;
 }
