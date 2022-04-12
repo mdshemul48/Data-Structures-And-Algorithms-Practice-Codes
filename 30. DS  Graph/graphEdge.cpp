e47246173f84ed23892e2ab374bf2dd294fba51a
MD. Shimul <mdshemul480@gmail.com>
2022-04-12 20:22:45 +0600

    adding graphEdge code

--- 30. DS  Graph/graphEdge.cpp
+++ 30. DS  Graph/graphEdge.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// aikane amara fist a akta note dhore oita print kortaci and oitar jono visited true kortaci.
+// then amara oitar next node a jaitaci. then oita print kore dhaktaci ja oitar next a kono node ase nake.
+// jodi thake tahole oitai jabo. aibabe cholte thakba..
+void print(vector<vector<int>> v, int sv, vector<bool> &visited)
+{
+
+    cout << sv << endl;
+    visited[sv] = true;
+
+    int n = v.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (v[sv][i] == 1 && visited[i] == false)
+        {
+            print(v, i, visited);
+        }
+    }
+}
+
 int main()
 {
     int n, e;
@@ -8,13 +27,14 @@ int main()
 
     vector<vector<int>> matrix(n, vector<int>(n, 0));
 
-    for (int i = 0; i <= e; i++)
+    for (int i = 1; i <= e; i++)
     {
         int fv, sv;
         cin >> fv >> sv;
         matrix[fv][sv] = 1;
         matrix[sv][fv] = 1;
     }
-
+    vector<bool> visited(n, 0);
+    print(matrix, 0, visited);
     return 0;
 }
